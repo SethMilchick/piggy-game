5c3f6d635f2ecd62f7e7397cad03226ca3e5d1bd
SethMilchick <martiskalenda@gmail.com>
2026-06-23 12:09:20 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const diceImages = [
+  new URL('./dice-1.png', import.meta.url).href,
+  new URL('./dice-2.png', import.meta.url).href,
+  new URL('./dice-3.png', import.meta.url).href,
+  new URL('./dice-4.png', import.meta.url).href,
+  new URL('./dice-5.png', import.meta.url).href,
+  new URL('./dice-6.png', import.meta.url).href,
+];
+
+// Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+const modal = document.querySelector('.modal');
+const rulesBtn = document.querySelector('.rules-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+};
+
+rulesBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+
+let scores, currentScore, activePlayer, playing;
+
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  diceEl.src = dice1;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    diceEl.classList.remove('hidden');
+    diceEl.src = diceImages[dice - 1];
+
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add('hidden');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
